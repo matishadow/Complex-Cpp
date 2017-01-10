8ea88581494f1c80391d2b805df4f6b0a23e9ada
matishadow <matishadow@gmail.com>
2017-01-10 23:32:06 +0100

    passing arguments using template

--- Complex.h
+++ Complex.h
@@ -4,10 +4,14 @@
 #include <type_traits>
 
 template<typename T,
+        T real = 0,
+        T imaginary = 0,
         typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 class Complex {
 public:
     Complex(T Real, T Imaginary) : Real(Real), Imaginary(Imaginary) {}
+
+    Complex() : Real(real), Imaginary(imaginary) {}
 
     T getImaginary() const {
         return Imaginary;
