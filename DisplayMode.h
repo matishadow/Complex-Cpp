e82e0227cd1874f0f74be1e42137d111f74a7eaa
matishadow <matishadow@gmail.com>
2017-01-11 21:13:53 +0100

    DisplayMode enum add

---
+++ DisplayMode.h
@@ -0,0 +1,10 @@
+#ifndef COMPLEX_CPP_DISPLAYMODE_H
+#define COMPLEX_CPP_DISPLAYMODE_H
+
+enum DisplayMode {
+    cartesianForm,
+    polarForm,
+    exponentialForm
+};
+
+#endif //COMPLEX_CPP_DISPLAYMODE_H
